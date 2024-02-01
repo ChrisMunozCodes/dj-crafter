7cf756b901c069b559c8a40ee8acd18220f5f6c8
Christopher Munoz <christopherm0507@gmail.com>
2024-01-31 20:32:19 -0500

    UI Updates to main page

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -89,25 +89,39 @@ onMounted(() => {
 
 
 <template>
-<div class="min-h-screen bg-green-400 flex justify-center items-center box-border flex-col">
-    <section class="prose">
-        <h2>{{ isPlaying ? 'Now Playing ' + playlist[currentSong].title : playlist[currentSong].title + ' is Paused' }}</h2>
+    <section class="flex min-w-screen min-h-screen">
+        <div class="bg-gray-600 min-h-screen w-1/5">
+            <div class="h-20 bg-gray-400 flex items-center justify-center">
+                <button class="btn">
+                UPLOAD
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 15v4c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2v-4M17 8l-5-5-5 5M12 4.2v10.3"/></svg>
+                </button>
+            </div>
+            <div class="h-screen flex items-center justify-center">
+                <h3 class="prose">No playlists avaliable</h3>
+            </div>
+        </div>
+
+        <div class="min-h-screen bg-green-400 flex justify-center items-center box-border flex-col w-4/5">
+            <section class="prose">
+                <h2>{{ isPlaying ? 'Now Playing ' + playlist[currentSong].title : playlist[currentSong].title + ' is Paused' }}</h2>
+            </section>
+            <section>
+            <audio id="player" ref="audioRef">
+                <source :src="playlist[currentSong].file" type="audio/mpeg">
+                Your browser does not support the audio element.
+            </audio>
+            <ul class="flex m-2">
+                <li><button class="btn btn-active btn-neutral prose m-2" @click="previousSong"> &lt; </button></li>
+                <li><button class="btn btn-active btn-accent prose m-2" @click="pausePlay">{{ isPlaying ? 'Pause' : 'Play' }}</button></li>
+                <li><button class="btn btn-active btn-neutral prose m-2" @click="nextSong"> &gt; </button></li>
+            </ul>
+            </section>
+            <section>
+                <progress id="seekbar" class="progress progress-green-400 w-56" value="0" max="100"></progress>
+            </section>
+        </div>
     </section>
-    <section>
-      <audio id="player" ref="audioRef">
-        <source :src="playlist[currentSong].file" type="audio/mpeg">
-        Your browser does not support the audio element.
-      </audio>
-      <ul class="flex m-2">
-        <li><button class="btn btn-active btn-neutral prose m-2" @click="previousSong"> &lt; </button></li>
-        <li><button class="btn btn-active btn-accent prose m-2" @click="pausePlay">{{ isPlaying ? 'Pause' : 'Play' }}</button></li>
-        <li><button class="btn btn-active btn-neutral prose m-2" @click="nextSong"> &gt; </button></li>
-      </ul>
-    </section>
-    <section>
-        <progress id="seekbar" class="progress progress-green-400 w-56" value="0" max="100"></progress>
-    </section>
-</div>
   </template>
 
   
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@
 
 <template>
         <nav className="navbar bg-black flex items-center z-20 drop-shadow-lg">
-            <h1 className="p-5 pl-2 text-3xl font-bold text-white prose">Welcome to DJ Crafter!</h1>
+            <h1 className="p-5 pl-2 text-3xl font-bold text-white prose">DJ Crafter</h1>
       </nav>
 </template>
 
